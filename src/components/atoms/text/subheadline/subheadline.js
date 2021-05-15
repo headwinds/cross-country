ed04b2cac15c62c2ad7de24cf35eeb19b0b25620
Brandon Flowers (LCL) <braflow@Brandon-Flowers.local>
2021-05-15 10:42:42 -0400

    progress on interactive grid

--- src/components/atoms/text/subheadline/subheadline.js
+++ src/components/atoms/text/subheadline/subheadline.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import styles from './subheadline.scss';
 import clsx from 'clsx';
 
-const SubHeadline = ({ text, size = 'small', children, customStyle = {}, customClass = '' }) => {
+const SubHeadline = ({ text, color = '', size = 'small', children, customStyle = {}, customClass = '' }) => {
   const key = `subheadline__${size}`;
   const className = `${styles.subheadline} ${styles[key]}`;
   return (
-    <h2 className={clsx(styles.subheadline, customClass)} style={customStyle}>
+    <h2 className={clsx(className, customClass)} style={{ color, ...customStyle }}>
       {text || children}
     </h2>
   );
