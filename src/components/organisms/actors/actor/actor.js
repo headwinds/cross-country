31e1bf0677957703ac03cd1af17963cbc8bbf4eb
Brandon Flowers (LCL) <braflow@Brandon-Flowers.local>
2021-09-19 10:50:14 -0400

    need overlay

--- src/components/organisms/actors/actor/actor.js
+++ src/components/organisms/actors/actor/actor.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import { Column } from '../../../';
 import styles from './actor.scss';
 import clsx from 'clsx';
 
-const Actor = ({ children, customClass = '', customStyle = {}, ...rest }) => (
-  <div className={clsx(styles.actor, customClass)} style={customStyle} {...rest}>
-    {children}
-  </div>
-);
+const Actor = ({ children, customClass = '', customStyle = {}, ...rest }) => {
+  return (
+    <Column>
+      <div className={clsx(styles.actor, customClass)} style={customStyle} {...rest}>
+        {children}
+      </div>
+    </Column>
+  );
+};
 
 export default Actor;
--- src/micro/games/frozen-lake/frozen-lake.js
+++ src/micro/games/frozen-lake/frozen-lake.js
@@ -1,9 +1,9 @@
-import React, { Component, useState } from 'react';
-import { Image, Column, SubHeadline, Wrapper, TileGrid, Tile, Paragraph, Modal } from '../../../components';
+import React, { Component, useState, useEffect } from 'react';
+import { Image, Column, SubHeadline, Wrapper, TileGrid, Tile, Paragraph, Modal, Hunter } from '../../../components';
 import PropTypes from 'prop-types';
 import { useFrozenLakeService } from './use-frozen-lake-service';
 
-const map = [
+const mock_map = [
   ['S', 'F', 'F', 'F'],
   ['F', 'H', 'F', 'H'],
   ['F', 'F', 'F', 'H'],
@@ -19,28 +19,48 @@ const colors = {
   G: palette[3],
 };
 
-const createDemoModels = total => {
+const createDemoModels = (total, generatedMap) => {
   const range = [...Array(total).keys()];
   let i = -1;
   let j = -1;
   return range.map(index => {
     if (index % 4 === 0) {
       i++;
-      j=-1;
+      j = -1;
     }
     j++;
-    const letter = map[i][j];
+    const letter = generatedMap[i][j];
     return { id: index, letter, fill: colors[letter] };
   });
 };
 
 const FrozenLake = ({ isStandalone = false, palette = null }) => {
-  const demoModels = createDemoModels(16);
+  const [demoModels, setDemoModels] = useState(createDemoModels(0, []));
   const [hasModal, toggleModal] = useState(false);
   const [selectedTile, toggleSelectedTile] = useState(null);
   const [current, send] = useFrozenLakeService();
 
   console.log('demoModels: ', demoModels);
+  console.log('rendering: ');
+
+  const {
+    context: { generatedMap },
+  } = current;
+  if (generatedMap) {
+    //setDemoModels(16, map);
+  }
+
+  useEffect(() => {
+    const {
+      context: { generatedMap },
+    } = current;
+
+    if (generatedMap?.length > 0) {
+      console.log('current changed: ', generatedMap);
+      const newDemoModels = createDemoModels(16, generatedMap);
+      setDemoModels(newDemoModels);
+    }
+  }, [current]);
 
   const renderModal = () => {
     if (hasModal) {
@@ -52,12 +72,41 @@ const FrozenLake = ({ isStandalone = false, palette = null }) => {
     }
   };
 
+  const renderGrid = () => {
+    return (
+      <>
+        <div
+          style={{
+            position: 'absolute',
+            zIndex: 1,
+            backgroundColor: 'transparent',
+            opacity: 1,
+            width: 600,
+            height: 400,
+          }}
+        >
+          <div style={{ position: 'absolute', zIndex: 0, left: 20, top: 120 }}>
+            <Hunter />
+          </div>
+        </div>
+        <TileGrid
+          models={demoModels}
+          totalInRow={4}
+          Tile={Tile}
+          isIsometric={true}
+          palette={palette}
+          tileConfig={{ size: 100, cornerColor: '#999' }}
+        />
+      </>
+    );
+  };
+
   if (isStandalone) {
     return (
       <Wrapper>
         <Column hasChildrenCentered>
           {renderModal()}
-          <TileGrid models={demoModels} totalInRow={4} Tile={Tile} isIsometric={true} palette={palette} />
+          {renderGrid()}
         </Column>
       </Wrapper>
     );
@@ -65,8 +114,7 @@ const FrozenLake = ({ isStandalone = false, palette = null }) => {
     return (
       <Column hasChildrenCentered>
         {renderModal()}
-        <div>{current.context.value}</div>
-        <TileGrid models={demoModels} totalInRow={4} Tile={Tile} isIsometric={true} />
+        {renderGrid()}
       </Column>
     );
   }
