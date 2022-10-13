475b17f4492c98e8839e5e64ce0eb273eb4c162f
Brandon Flowers (LCL) <braflow@LLC02XV21ZJGH5.ngco.com>
2022-10-12 22:08:47 -0400

    wordle machine

--- src/components/organisms/word-search/word-search.js
+++ src/components/organisms/word-search/word-search.js
@@ -1,30 +1,30 @@
 import React, { useEffect } from 'react';
 import { Image, Column, SubHeadline } from '../../';
 import styles from './word-search.scss';
-import { wordSearchMachine } from './word-search-machine';
+import { wordleSearchMachine } from './wordle-search-machine';
 //import { searchMachine } from './search-machine';
 import { useMachine, useInterpret, useSelector } from '@xstate/react';
-import { Words } from './words';
+import { WordleHero } from './wordle-hero';
 import clsx from 'clsx';
 
 const WordSearch = ({ config: { text, hasBackground } }) => {
-  const [current, send] = useMachine(wordSearchMachine);
+  const [current, send] = useMachine(wordleSearchMachine);
   const { context } = current;
-  const { subredditService, subredditServices } = context;
+  const { wordleService, wordleServices } = context;
 
-  const posts = [{ author: 'test', title: 'test', id: 'test' }];
+  const posts = [{ author: 'Princess Rhaenyra Targaryen', title: 'test', id: 'test' }];
 
   const firstPost = posts?.[0] ?? { id: 0 };
   const author = posts?.[0]?.author ?? 'Princess Rhaenyra Targaryen';
 
   useEffect(() => {
-    console.log('WordSearch -> useEffect - did current get subreddit?! ', context);
+    console.log('WordSearch -> useEffect - context: ', context);
   }, [context]);
 
   return (
     <Column hasBackground={hasBackground}>
       <SubHeadline text={`${text} ${author}`} />
-      {subredditService && <Words service={subredditService} key={subredditService.id} />}
+      {wordleService && <WordleHero service={wordleService} key={wordleService.id} />}
     </Column>
   );
 };
