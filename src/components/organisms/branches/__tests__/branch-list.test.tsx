195fb14f4e6ece1606916e86f854bf4a461c1257
Brandon Flowers (LCL) <braflow@LLC02XV21ZJGH5.ngco.com>
2022-12-15 11:54:09 -0500

    chore: branches is typescript with tests

---
+++ src/components/organisms/branches/__tests__/branch-list.test.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { createAllPortholeTrees, getRSSBranch, convertToPortholeBranches } from '../../../../utils/golds/feed-util';
+import { getAllItemsFromStore } from '../../../../utils/golds/indexdb-util';
+import { differenceBy, shuffle } from '../../../../utils/fp-util';
+
+import { BranchType } from '../../../../types/branch.type';
+import { BranchListProps } from '../branch-list.types';
+import { response } from './branches-mock';
+import BranchList from '../branch-list';
+
+const allNewBranches = convertToPortholeBranches(response.feed_responses);
+const shuffledBranches = shuffle(allNewBranches);
+
+// ensure all branches are unique
+const uniqueBranches = [...new Set<BranchType>(shuffledBranches)];
+
+describe('Test Component', () => {
+  let props: BranchListProps;
+
+  beforeEach(() => {
+    props = {
+      branches: uniqueBranches,
+    };
+  });
+
+  const renderComponent = () => render(<BranchList {...props} />);
+
+  it('should render foo text correctly', () => {
+    const { getByTestId } = renderComponent();
+
+    const component = getByTestId('branch-list');
+
+    expect(component).toBeTruthy();
+  });
+});
--- src/components/organisms/branches/branches.types.ts
+++
@@ -1,3 +0,0 @@
-export interface BranchesProps {
-    foo: string;
-}
