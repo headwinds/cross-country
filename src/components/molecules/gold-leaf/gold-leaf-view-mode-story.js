c6474fdf44d5f55a13dc8c46578e3e29c4e80ba9
Brandon Flowers (LCL) <braflow@LLC02XV21ZJGH5.ngco.com>
2023-01-26 13:17:14 -0500

    chore: sorting out gold view flow

---
+++ src/components/molecules/gold-leaf/gold-leaf-view-mode-story.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import GoldLeaf from './';
+import getImagesFromDescription from '../../../utils/golds/image-find-util';
+import { getRSSBranch } from '../../../utils/golds/feed-util';
+import behanceBranches from './behance-raw-branches';
+
+const GoldLeafViewModeStory = ({ config }) => {
+  const index = 0;
+  const convertToPortholeBranches = branches => {
+    // raw
+    const newBranches = branches;
+    // refined
+    const portholeBranches = newBranches.map((candidateBranch, ix) => {
+      return getRSSBranch(candidateBranch, index, ix);
+    });
+    // validated
+    const portholeBranchesValid = portholeBranches.filter(branch => branch !== null);
+
+    return portholeBranchesValid;
+  };
+
+  const portholeBranchModels = convertToPortholeBranches(behanceBranches);
+  const goldLeafModel = portholeBranchModels[0];
+  console.log('RSS consumption goldLeafModel: ', goldLeafModel);
+  return <GoldLeaf mode="view" goldLeafModel={goldLeafModel} config={config} />;
+};
+
+export default GoldLeafViewModeStory;
