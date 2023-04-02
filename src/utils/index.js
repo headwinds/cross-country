1681241e9506f0d9c39461c0557aae3c6051e8ff
Brandon Flowers <brandonflowers@gmail.com>
2023-04-02 10:08:23 -0400

    fix: export of fp util

--- src/utils/index.js
+++ src/utils/index.js
@@ -6,3 +6,6 @@ export { default as feedUtil } from './golds/feed-util';
 
 // colour & palettes
 export { default as ColourUtil } from './color-util';
+
+// shuffle
+export { default as FpUtil } from './fp-util';
