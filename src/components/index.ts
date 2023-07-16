1788f6428faf7f3140a92945971e14cdb4d21d42
Brandon Flowers <brandonflowers@gmail.com>
2023-07-16 13:42:00 -0400

    list item same colour

--- src/components/index.ts
+++ src/components/index.ts
@@ -53,8 +53,8 @@ export { default as KeyValuePairs } from './molecules/key-value-pairs';
 export { default as Loading } from './molecules/loading';
 export { default as Error } from './molecules/error';
 
-//export { default as PrivacyPolicy } from './molecules/policies/privacy-policy';
-//export { default as TermsOfUse } from './molecules/policies/terms-of-use';
+export { default as PrivacyPolicy } from './molecules/policies/privacy-policy';
+export { default as TermsOfUse } from './molecules/policies/terms-of-use';
 
 // organisms
 export { default as Login } from './organisms/login';
