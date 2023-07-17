95979bd83c8954802b38370a9f62fb9726b94f45
Brandon Flowers <brandonflowers@gmail.com>
2023-07-17 15:21:22 -0400

    bolt magic

---
+++ src/components/pages/home/headwinds/bolt.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Image } from '../../..';
+
+import bolt from './bolt.svg';
+
+export const Bolt = () => <Image a11y="lightening bolt" url={bolt} width={30} height={30} />;
