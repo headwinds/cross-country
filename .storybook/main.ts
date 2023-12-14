62793ba041535710e756a37e9ed0db3f8923c9bd
Brandon Flowers <brandonflowers@nicks-MBP-2.lan>
2023-12-13 20:52:04 -0500

    fix: lots of impoorts

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,8 +2,8 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+    "../lib/**/*.mdx",
+    //"../lib/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
