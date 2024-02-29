ab83fc47ddab4288840590e538e370a61a1e2b20
Brandon Flowers <brandonflowers@nicks-MBP-2.lan>
2024-02-29 17:01:36 -0500

    chore: story to prove how xstate works with invoking a fetch

--- lib/components/atoms/text/input/state-machine-text-input.tsx
+++ lib/components/atoms/text/input/state-machine-text-input.tsx
@@ -1,19 +1,17 @@
 import React from "react";
 import { useMachine } from "@xstate/react";
-import { createMachine, assign, actions } from "xstate";
+import { createMachine, assign } from "xstate";
 import styles from "./text-input.module.css";
 
 import clsx from "clsx";
 
-const { send, cancel } = actions;
-
-// Define the machine
 const inputMachine = createMachine(
   {
     id: "input",
     initial: "typing",
     context: {
       inputValue: "",
+      send: undefined, // Add send as a context property
     },
     states: {
       typing: {
@@ -32,34 +30,33 @@ const inputMachine = createMachine(
     on: {
       DEBOUNCED_INPUT: {
         actions: assign({
-          inputValue: (context, event) => event.value,
+          inputValue: (_, event) => event.value,
         }),
       },
     },
   },
   {
     actions: {
-      debounceInput: send(
-        (_, event) => ({
-          type: "DEBOUNCED_INPUT",
-          value: event.value,
-        }),
-        {
-          delay: 200, // Debounce time in milliseconds
-          id: "input-debounce", // Unique ID to cancel the previous debounce event
-        }
-      ),
-      cancelDebounce: cancel("input-debounce"),
+      debounceInput: (context) => {
+        context.send("DEBOUNCED_INPUT", {
+          value: context.inputValue,
+        });
+      },
     },
   }
 );
 
-export const InputField = ({ customClass = "", customStyle = {} }) => {
+const InputField = ({ customClass = "", customStyle = {}, placeholder }) => {
   const [state, send] = useMachine(inputMachine);
 
   const handleChange = (event) => {
     send("INPUT_CHANGE", { value: event.target.value });
   };
+
+  // Update the context with the send function
+  React.useEffect(() => {
+    send({ type: "xstate.assign", property: "send", value: send });
+  }, [send]);
 
   return (
     <input
@@ -68,6 +65,9 @@ export const InputField = ({ customClass = "", customStyle = {} }) => {
       className={clsx(styles.textInput, customClass)}
       style={customStyle}
       onChange={handleChange}
+      placeholder={placeholder}
     />
   );
 };
+
+export default InputField;
