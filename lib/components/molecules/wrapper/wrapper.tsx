44f07c5a2cc5828f3e2e36a43ec6d876277497a5
Brandon Flowers <brandonflowers@nicks-mbp-2.home>
2024-04-03 09:13:48 -0400

    card link

--- lib/components/molecules/wrapper/wrapper.tsx
+++ lib/components/molecules/wrapper/wrapper.tsx
@@ -1,20 +1,30 @@
-import React, { forwardRef } from 'react';
-import Column from '../../atoms/column/column';
-import styles from './wrapper.module.css';
-import clsx from 'clsx';
+import React, { forwardRef } from "react";
+import Column from "../../atoms/column/column";
+import styles from "./wrapper.module.css";
+import clsx from "clsx";
 
 // TODO: add types
-const Wrapper = forwardRef(({ backgroundColor = '', customClass = '', customStyle = {}, ...rest }: any, ref) => {
-  return (
-    <Column
-      {...rest}
-      customClass={clsx(styles.wrapper, customClass)}
-      customStyle={{
-        backgroundColor,
-        ...customStyle,
-      }}
-    ></Column>
-  );
-});
+const Wrapper = forwardRef(
+  (
+    {
+      backgroundColor = "",
+      customClass = "",
+      customStyle = { margin: 0, padding: 0 },
+      ...rest
+    }: any,
+    ref
+  ) => {
+    return (
+      <Column
+        {...rest}
+        customClass={clsx(styles.wrapper, customClass)}
+        customStyle={{
+          backgroundColor,
+          ...customStyle,
+        }}
+      ></Column>
+    );
+  }
+);
 
 export default Wrapper;
