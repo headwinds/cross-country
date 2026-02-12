c86f44ab0fbfc548a6405052f9bbbe6869b721f8
brandon flowers <brandon.flowers@gmail.com>
2026-02-12 08:41:45 -0500

    forget export types

--- lib/components/molecules/index.ts
+++ lib/components/molecules/index.ts
@@ -36,7 +36,8 @@ export { default as User } from "./user";
 export type { UserProps } from "./user";
 
 export { default as KeyValuePairs } from "./key-value-pairs";
-export type { KeyValuePairsProps } from "./key-value-pairs";
+export type { KeyValuePairsProps, KeyValue } from "./key-value-pairs";
+export { VALUE_TYPE, KEY_VALUE_EVENTS } from "./key-value-pairs";
 
 export { default as Loading } from "./loading";
 export type { LoadingProps } from "./loading";
